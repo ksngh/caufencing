71c8d53fea1bb970a86f170035be4317ea970531
ksngh <chzhqk98@naver.com>
2024-01-24 17:23:42 +0900

    initial commit

---
+++ src/main/java/caufencing/caugarde/CaugardeApplication.java
@@ -0,0 +1,13 @@
+package caufencing.caugarde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaugardeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaugardeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/caufencing/caugarde/CaugardeApplicationTests.java
@@ -0,0 +1,13 @@
+package caufencing.caugarde;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaugardeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
